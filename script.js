9246117624e3ea634b010cdfdb3d746b6b93bb53
Roger Mullins <rogermullins.mba@gmail.com>
2022-09-02 04:17:49 +0000

    Added interactive section

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 let siteFeedback = [
-    "I thought that would be bigger news.",
+    "Well that's strange. I thought that would be bigger news.",
     "Well, there seems to be an absence here of a certain... ornithological piece. An article regarding mass awareness of a certain... avian variety.",
     "Oh, have you not heard? It was my understanding that everyone had heard."
 ];
 
 let buttonContents = [
-    "You thought what would be big news?",
+    "You thought what would be bigger news?",
     "What are you talking about?",
     "Heard what?"
 ];
 
 let conversationPoint = 0;
 
-let interactiveDiv = document.getElementById('interactiveContent');
-
-function interact(conversationPoint) {
-    console.log("Time through/Corresponding index: ", conversationPoint);
+function interact() {
+    // console.log("Time through/Corresponding index: ", conversationPoint);
     let divAreaText = document.getElementById('feedback');
     let divAreaButton = document.getElementById('button');
-    // divAreaButton.removeAttribute('click');
+    
+    if (conversationPoint == 3) {
+        let surfinBird = document.createElement('img');
+        surfinBird.src = "imgs/peter-griffin-family-guy.gif";
+        divAreaText.textContent = "";
+        divAreaButton.remove();
+        divAreaText.appendChild(surfinBird);
+    } else {
+ 
     divAreaText.textContent = siteFeedback[conversationPoint];
-    // document.appendChild(divAreaText);
     divAreaButton.textContent = buttonContents[conversationPoint];
-    // divAreaButton.addEventListener('click', 'interact()');
-    // interactiveDiv.appendChild(divAreaText);
-    // interactiveDiv.appendChild(divAreaButton);
     conversationPoint++;
+    }
 }
